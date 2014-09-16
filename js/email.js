ff351988ac669b0686a9c3dd41bcfe382abfbbec
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-16 02:19:08 -0400

    upd infos + email script

---
+++ js/email.js
@@ -0,0 +1,4 @@
+var arrKeys = ["thieu", "ma", 64, "rhemes.com", "coding"];
+
+document.getElementById("secretemail").textContent =
+    arrKeys[1] + arrKeys[0] + String.fromCharCode(arrKeys[2]) + arrKeys[4] + arrKeys[3];
