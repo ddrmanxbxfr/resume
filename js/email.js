dbb2c63a71dde330dc95adbec81a6bffbc31cf6b
Mathieu Rheaume <mathieu@codingrhemes.com>
2017-01-16 01:54:42 -0500

    Fixed the linkedin link and update my pdf reusme

--- js/email.js
+++ js/email.js
@@ -1,4 +1,2 @@
 var arrKeys = ["thieu", "ma", 64, "rhemes.com", "coding"];
-
-document.getElementById("secretemail").textContent =
-    arrKeys[1] + arrKeys[0] + String.fromCharCode(arrKeys[2]) + arrKeys[4] + arrKeys[3];
+document.getElementById("secretemail").textContent = arrKeys[1] + arrKeys[0] + String.fromCharCode(arrKeys[2]) + arrKeys[4] + arrKeys[3];
